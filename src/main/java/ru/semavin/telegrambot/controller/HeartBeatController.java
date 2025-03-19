ab335425c0307e9167db680f63709eebb07288d9
asem25 <asemavin250604@gmail.com>
2025-03-19 19:04:43 +0300

    add check HeartBeatController

---
+++ src/main/java/ru/semavin/telegrambot/controller/HeartBeatController.java
@@ -0,0 +1,20 @@
+package ru.semavin.telegrambot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class HeartBeatController {
+    @GetMapping("/check")
+    public ResponseEntity<String> heartbeat() {
+        log.info("Checking heartbeat at: {}", LocalDate.now());
+        return ResponseEntity.ok("API is alive");
+    }
+}
